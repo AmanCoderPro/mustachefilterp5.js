745ab865e552de006b9842505ee20f6758de7579
AmanCoderPro <87359434+AmanCoderPro@users.noreply.github.com>
2022-09-30 17:14:40 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,15 @@ function preload() {
 function setup() {
     canvas = createCanvas(300, 300);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();   
+    poseNet = ml5.poseNet(video, modelLoaded); 
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet is initialised');
 }
 
 function draw() {
@@ -12,4 +21,12 @@ function draw() {
 
 function take_snapshot() {
     save('MyFilteredFaceImage.png');
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+        console.log('nose x = ' + results[0].pose.nose.x);
+        console.log('nose y = ' + results[0].pose.nose.y);
+    }
 }
